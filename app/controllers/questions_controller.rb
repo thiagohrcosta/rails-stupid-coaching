6ff8cf2984311adea3013bd79b1fd8d585886965
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-08 15:33:55 -0300

    Code refactor

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,17 @@ class QuestionsController < ApplicationController
 
   def answer
     @answer = params[:question]
+    @coach_answer = question(@answer)
   end
+
+  def question(answer)
+    if @answer.match(/\w*\?{1}/)
+      return "Silly question, get dressed and go to work!"
+    elsif @answer.match(/I am going to work/)
+      return "Great"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
 end
